96b113600d98f7e697f3f2d7fff2c08961efd288
rogerio14698 <rogeriolucas14698@gmail.com>
2026-06-09 19:20:58 +0200

    Cambios realizados: traspaso de los archivos css a scss en sus respectivas anidaciones y modificado el gulpfile.js instalando nuevos plugins para mejorar la vida del proyecto y optimizarlo

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
 
 function compileSass() {
-  return src('./sass/**/*.scss')
+  return src(['./sass/**/*.scss', './sass/**/*.sass']) // Soporta ambas extensiones
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
     .pipe(dest('./public/css'));
 }
 
 function watchSass() {
-  watch('./sass/**/*.scss', compileSass);
+  watch(['./sass/**/*.scss', './sass/**/*.sass'], compileSass);
 }
 
 exports.compile = compileSass;
