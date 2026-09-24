79567376238471fdda33564ec5648eec52627583
rogerio14698 <rogeriolucas14698@gmail.com>
2026-09-24 14:02:28 +0200

    fix: Update login form action to use the correct route for user authentication

--- resources/views/components/blog.blade.php
+++ resources/views/components/blog.blade.php
@@ -24,7 +24,7 @@
 
             <div class="login">
                 <p>Inicio Sesión</p>
-                <form action="{{ route('login') }}" method="POST">
+                <form action="#" method="POST">
                     @csrf
                     <input type="text" name="user" placeholder="Usuario" required>
                     <input type="password" name="password" placeholder="Contraseña" required>
